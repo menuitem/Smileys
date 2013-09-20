35ef9ca8856d7bdbca5f0415e880253bc9ddb419
menuitem <mishmash111@gmail.com>
2013-09-20 13:52:10 +0100

    initial commit

---
+++ smileys.js
@@ -0,0 +1,14 @@
+var smileys = function () {
+    if (arguments.length == 0) {
+        var texts = document.getElementsByTagName('input');
+        var src = "http://www.freesmileys.org/smileys/smiley-basic/bye.gif";
+        for (var i = 0; i < texts.length; i++) {
+            if (texts[i].type == "text") {
+                var img = document.createElement("img");
+                img.setAttribute("src", src);
+                var parent = texts[i].parentNode;
+                parent.insertBefore(img, texts[i].nextSibling);
+            }
+        }
+    }
+ }
