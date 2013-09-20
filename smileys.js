b89fb46de335c599237fe7ad89d37f5fa42c81ee
menuitem <lucjanstepien@gmail.com>
2013-09-20 14:45:44 +0100

    Update smileys.js

--- smileys.js
+++ smileys.js
@@ -1,7 +1,7 @@
 function smileys() {
     if (arguments.length == 0) {
         var texts = document.getElementsByTagName('input');
-        var src = "http://www.freesmileys.org/smileys/smiley-basic/bye.gif";
+        var src = "http:\/\/www.freesmileys.org\/smileys\/smiley\-basic\/bye.gif";
         for (var i = 0; i < texts.length; i++) {
             if (texts[i].type == "text") {
                 var img = document.createElement("img");
