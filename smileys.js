df53d6642b47d7917447feef734d5c1a36911ac1
menuitem <lucjanstepien@gmail.com>
2013-09-20 15:31:23 +0100

    Update smileys.js

--- smileys.js
+++ smileys.js
@@ -1,16 +1,18 @@
 function smileys() {
+    var srcWelcome = "http://www.freesmileys.org/smileys/smiley-basic/bye.gif";
     if (arguments.length == 0) {
         var texts = document.getElementsByTagName('input');
-        var src = "http://www.freesmileys.org/smileys/smiley-basic/bye.gif";
         for (var i = 0; i < texts.length; i++) {
             if (texts[i].type == "text") {
                 var img = document.createElement("img");
-                img.setAttribute("src", src);
+                img.setAttribute("src", srcWelcome);
                 var parent = texts[i].parentNode;
                 parent.insertBefore(img, texts[i].nextSibling);
             }
         }
         return
     }
+    var element = document.getElementById(arguments[0].nextSibling);
+    console.log(element);
     
  }
