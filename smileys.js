8154e1e3d10eb626ad167df9fab02aff3434526a
menuitem <lucjanstepien@gmail.com>
2013-09-20 19:40:06 +0100

    Update smileys.js

--- smileys.js
+++ smileys.js
@@ -1,5 +1,7 @@
-function smileys(arguments) {
+function smileys() {
     var srcWelcome = "http://www.freesmileys.org/smileys/smiley-basic/bye.gif";
+    var srcGood = "http://www.freesmileys.org/smileys/smiley-basic/biggrin.gif";
+    var srcBad = "http://www.freesmileys.org/smileys/smiley-basic/rofl.gif";
     if (arguments.length == 0) {
         var texts = document.getElementsByTagName('input');
         for (var i = 0; i < texts.length; i++) {
@@ -12,7 +14,21 @@ function smileys(arguments) {
         }
         return
     }
-    var element = document.getElementById(arguments[0].nextSibling);
-    console.log(element);
-    
+    if (arguments.length == 1){
+        var element = arguments[0]
+        var img = arguments[0].nextSibling;
+        console.log(element.value)
+        if (element.value==""){
+            img.setAttribute('src', srcWelcome);
+            return
+        }else{
+            img.setAttribute('src', srcGood);
+            return
+        }
+    }
+    if (arguments.length == 2){
+        var element = arguments[0]
+        var img = arguments[0].nextSibling;
+        img.setAttribute('src', srcBad);
+    }
  }
